3c7d07356c570bbf5079e867660cca8c1f3c1aaa
Matt Wong <mattwong59@gmail.com>
2019-03-04 18:38:16 -0800

    Add working filters, and ability to add new todo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { createStore } from 'redux';
+import todoApp from './reducers'
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const store = createStore(todoApp)
+
+render(
     <Provider store={store}>
         <App />
     </Provider>, 
